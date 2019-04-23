5df21a474ca24d0c08dcd291b0fa55d4bbbe12fd
shohan494 <shohan12224943@gmail.com>
2019-04-23 10:00:47 +0600

    bulma sample html trial

--- vue-component.js
+++ vue-component.js
@@ -1,11 +1,29 @@
+Vue.component('tasklist', {
+  
+  data: function () {
+    return {
+      tasks: [		
+  				{description: 'Go to school', completed: true},
+				{description: 'Go to college', completed: false},
+				{description: 'Go to store', completed: true},
+				{description: 'Go to home', completed: false}
+			]
+    };
+  },
+  template: '<div><task v-for="task in tasks">{{task.description}}</task></div>'
+
+})
+
+
 Vue.component('task', {
 
 template: '<li><slot></slot></li>'
 
-})
+});
+
 
 new Vue({
 	el: "#root"
+});
 
 
-})
